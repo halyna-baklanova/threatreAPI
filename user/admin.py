1723fd1ad38775e03e7492b5f1b8d3d1bb3c24ba
halyna-baklanova <novabalka@gmail.com>
2025-01-11 14:27:00 +0200

    fix auth for user

--- user/admin.py
+++ user/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_staff')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
 
 admin.site.register(User, CustomUserAdmin)
